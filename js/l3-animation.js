9095e6980da94a147e72eba8e6e3a5acd4ffaabc
damiengo <dam.gou@gmail.com>
2014-01-10 15:18:54 +0100

    Answer

--- js/l3-animation.js
+++ js/l3-animation.js
@@ -46,7 +46,7 @@ var setup = function() {
     // Afterwards, call setInterval to run at a framerate of 30 frames 
     // per second, calling the animate function each time.
     // YOUR CODE HERE
-    var assetsLen = assets.ength;
+    var assetsLen = assets.length;
     for(var i=0 ; i<assetsLen ; i++) {
         img = new Image();
         img.onload = onImageLoad;
@@ -65,7 +65,88 @@ var animate = function(){
     // Try your code with this call to clearRect commented out
     // and uncommented to see what happens!
     //
-    //ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+  
+    // YOUR CODE HERE
+    ctx.drawImage(frames[current], 0, 0);
+    current++;
+    if(current == frames.length) {
+        current = 0;
+    }
+};
+
+// We'll call your setup function in our test code, so
+// don't call it in your code.
+// setup();
+
+
+
+
+var canvas = null;
+var ctx = null;
+var current = 0;
+var assets = ['/media/img/gamedev/robowalk/robowalk00.png',
+              '/media/img/gamedev/robowalk/robowalk01.png',
+              '/media/img/gamedev/robowalk/robowalk02.png',
+              '/media/img/gamedev/robowalk/robowalk03.png',
+              '/media/img/gamedev/robowalk/robowalk04.png',
+              '/media/img/gamedev/robowalk/robowalk05.png',
+              '/media/img/gamedev/robowalk/robowalk06.png',
+              '/media/img/gamedev/robowalk/robowalk07.png',
+              '/media/img/gamedev/robowalk/robowalk08.png',
+              '/media/img/gamedev/robowalk/robowalk09.png',
+              '/media/img/gamedev/robowalk/robowalk10.png',
+              '/media/img/gamedev/robowalk/robowalk11.png',
+              '/media/img/gamedev/robowalk/robowalk12.png',
+              '/media/img/gamedev/robowalk/robowalk13.png',
+              '/media/img/gamedev/robowalk/robowalk14.png',
+              '/media/img/gamedev/robowalk/robowalk15.png',
+              '/media/img/gamedev/robowalk/robowalk16.png',
+              '/media/img/gamedev/robowalk/robowalk17.png',
+              '/media/img/gamedev/robowalk/robowalk18.png'
+             ];
+var frames = [];
+
+var onImageLoad = function(){
+    console.log("IMAGE!!!");
+};
+
+var setup = function() {
+    body = document.getElementById('body');
+    canvas = document.createElement('canvas');
+
+    ctx = canvas.getContext('2d');
+    
+    canvas.width = 100;
+    canvas.height = 100;
+
+    body.appendChild(canvas);
+
+    // Load each image URL from the assets array into the frames array 
+    // in the correct order.
+    // Afterwards, call setInterval to run at a framerate of 30 frames 
+    // per second, calling the animate function each time.
+    // YOUR CODE HERE
+    var assetsLen = assets.length;
+    for(var i=0 ; i<assetsLen ; i++) {
+        img = new Image();
+        img.onload = onImageLoad;
+        img.src = assets[i];
+        frames[i] = img;
+    }
+
+    setInterval(function(){ animate(); }, 33.3333);
+};
+
+var animate = function(){
+    // Draw each frame in order, looping back around to the 
+    // beginning of the animation once you reach the end.
+    // Draw each frame at a position of (0,0) on the canvas.
+  
+    // Try your code with this call to clearRect commented out
+    // and uncommented to see what happens!
+    //
+    ctx.clearRect(0,0,canvas.width, canvas.height);
   
     // YOUR CODE HERE
     ctx.drawImage(frames[current], 0, 0);
